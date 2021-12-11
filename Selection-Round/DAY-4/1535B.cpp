091cbbf0d89d29e3aee87a3c4e09756c4253ab45
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2021-12-11 12:46:43 +0530

    Create 1535B.cpp

---
+++ Selection-Round/DAY-4/1535B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+    sort(a.begin(), a.end(), [](int x, int y) {
+      return x % 2 < y % 2;
+    });
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = i + 1; j < n; ++j) {
+        ans += __gcd(a[i], a[j] * 2) > 1;
+      }
+    }
+    cout << ans << endl;
+  }
+}
