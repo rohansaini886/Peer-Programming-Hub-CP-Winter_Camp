47ad90b7b4800c44da65324ef1f829f690f33c55
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2021-12-11 12:47:08 +0530

    Create 1537B.pp

---
+++ Selection-Round/DAY-4/1537B.pp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m, i, j;
+        cin >> n >> m >> i >> j;
+
+        cout << 1 << " " << 1 << " " << n << " " << m << "\n";
+    }
+}
