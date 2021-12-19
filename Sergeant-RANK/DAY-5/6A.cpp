383c39c357f3fdfe82f906ab3946b6a709dab8bf
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2021-12-19 21:44:34 +0530

    Create 6A.cpp

---
+++ Sergeant-RANK/DAY-5/6A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+    int a,b,c,d;
+
+    while (cin >> a >> b >> c >> d)
+    {
+        if ((a < b+c && b < a+c && c < a+b) || (a < b+d && b < a+d && d < a+b) || (a < d+c && d < a+c && c < a+d) || (d < b+c && b < d+c && c < d+b))
+            cout << "TRIANGLE" << endl;
+        else if ((a == b+c || b == a+c || c == a+b) || (a == b+d || b == a+d || d == a+b) || (a == d+c || d == a+c || c == a+d) || (d == b+c || b == d+c || c == d+b))
+            cout << "SEGMENT" << endl;
+        else
+            cout << "IMPOSSIBLE" << endl;
+    }
+
+    return 0;
+}
