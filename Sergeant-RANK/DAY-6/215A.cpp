415302781b7d20a11508ed82727e6867d9bd52bc
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2021-12-21 21:36:40 +0530

    Create 215A.cpp

---
+++ Sergeant-RANK/DAY-6/215A.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m, a[50], b, max(0), count(1);
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+    cin >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> b;
+        for (int j = 0; j < n; ++j)
+        {
+            if (b % a[j] == 0)
+            {
+                int x = b / a[j];
+                if (x > max)
+                {
+                    max = x;
+                    count = 1;
+                }
+                else if (x == max)
+                {
+                    count += 1;
+                }
+                break;
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
